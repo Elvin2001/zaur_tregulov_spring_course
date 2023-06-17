4e9b96ee7628fee462498f9f2adea23b4df0afc7
Elvin Abaev <elvinabaev@gmail.com>
2023-06-17 06:43:07 +0300

    Dependency Injection through setter.

--- main/java/spring_introduction/Person.java
+++ main/java/spring_introduction/Person.java
@@ -3,8 +3,12 @@ package spring_introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Person: set pet");
         this.pet = pet;
     }
 
