d01daf8b48e2253df7619efe929040288355b982
Elvin Abaev <elvinabaev@gmail.com>
2023-06-19 07:23:42 +0300

    Using @Value in bean.

--- main/java/spring_introduction/ConfigWithAnnotations1.java
+++ main/java/spring_introduction/ConfigWithAnnotations1.java
@@ -6,10 +6,9 @@ public class ConfigWithAnnotations1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
 
-          Person person =  context.getBean("personBean", Person.class);
-          person.callYourPet();
-//        Cat myCat = context.getBean("cat", Cat.class);
-//        myCat.say();
+        Person person = context.getBean("personBean", Person.class);
+        System.out.println("Имя человека: " + person.getName());
+        System.out.println("возраст человека: " + person.getAge());
 
         context.close();
     }
--- main/java/spring_introduction/Person.java
+++ main/java/spring_introduction/Person.java
@@ -2,13 +2,20 @@ package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
     private Pet pet;
+
+    @Value("${person.surname}")
     private String surname;
+
+    @Value("${person.name}")
     private String name;
+
+    @Value("${person.age}")
     private int age;
 
     @Autowired
@@ -31,25 +38,25 @@ public class Person {
         return surname;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+//    public void setSurname(String surname) {
+//        this.surname = surname;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
 
     public void callYourPet() {
         System.out.println("Hello, my lovely pet!");
