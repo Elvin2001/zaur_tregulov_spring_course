66ef9b8edc8e158f4fabdead69da45bda542713e
Elvin Abaev <elvinabaev@gmail.com>
2023-06-21 22:07:37 +0300

    Pointcut with parameters.

--- main/java/aop/Test1.java
+++ main/java/aop/Test1.java
@@ -6,17 +6,10 @@ public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
+        Book book = context.getBean("book", Book.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
-
-        uniLibrary.getBook();
-        schoolLibrary.getBook();
-
-        uniLibrary.returnBook();
-        uniLibrary.getMagazine();
-
-        schoolLibrary.returnBook();
-        uniLibrary.returnMagazine();
+        uniLibrary.getBook(book);
+//      SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
 
         context.close();
     }
--- main/java/aop/UniLibrary.java
+++ main/java/aop/UniLibrary.java
@@ -3,13 +3,13 @@ package aop;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UniLibrary extends AbstractLibrary {
-    @Override
-    public void getBook() {
-        System.out.println("Мы берём книгу из UniLibrary");
+public class UniLibrary {
+//    @Override
+    public void getBook(Book book) {
+        System.out.println("Мы берём книгу из UniLibrary: " + book.getName());
     }
 
-    @Override
+//    @Override
     public void returnBook() {
         System.out.println("Мы возвращаем книгу из UniLibrary");
     }
