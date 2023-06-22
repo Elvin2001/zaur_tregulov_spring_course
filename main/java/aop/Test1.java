5f02c6faa6eda34a5a9988ff1addc479c62e6b5e
Elvin Abaev <elvinabaev@gmail.com>
2023-06-22 11:51:34 +0300

    Pointcut announcement.

--- main/java/aop/Test1.java
+++ main/java/aop/Test1.java
@@ -6,9 +6,10 @@ public class Test1 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
-        Book book = context.getBean("book", Book.class);
+//        Book book = context.getBean("book", Book.class);
         UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
-        uniLibrary.getBook(book);
+        uniLibrary.getBook();
+        uniLibrary.getMagazine();
 //      SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
 
         context.close();
