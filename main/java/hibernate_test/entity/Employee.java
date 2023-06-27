6dc28f1491359241253521edc27902797c25c9c8
Elvin Abaev <elvinabaev@gmail.com>
2023-06-27 19:43:34 +0300

    Сreating a relationship between a class and a table.

---
+++ main/java/hibernate_test/entity/Employee.java
@@ -0,0 +1,33 @@
+package hibernate_test.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+}
