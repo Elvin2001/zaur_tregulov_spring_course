8e43e500e086d6df0cfaf959c876f25856b52f14
Elvin Abaev <elvinabaev@gmail.com>
2023-06-29 13:34:16 +0300

    Bi-directional One-to-Many relations between Entities. Operations with objects.

---
+++ main/java/hibernate_one_to_many_bi/entity/Department.java
@@ -0,0 +1,63 @@
+package hibernate_one_to_many_bi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String departmentName;
+
+    @Column(name = "max_salary")
+    private int maxSalary;
+
+    @Column(name = "min_salary")
+    private int minSalary;
+
+    @OneToMany(mappedBy = "department",
+            cascade = {CascadeType.PERSIST, CascadeType.DETACH,
+                    CascadeType.MERGE, CascadeType.REFRESH})
+    private List<Employee> emps;
+
+    public Department() {
+    }
+
+    public Department(String departmentName, int maxSalary, int minSalary) {
+        this.departmentName = departmentName;
+        this.maxSalary = maxSalary;
+        this.minSalary = minSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", departmentName='" + departmentName + '\'' +
+                ", maxSalary=" + maxSalary +
+                ", minSalary=" + minSalary +
+                '}';
+    }
+
+    public void addEmployeeToDepartment(Employee emp) {
+        if (emps == null) {
+            emps = new ArrayList<>();
+        }
+        emps.add(emp);
+        emp.setDepartment(this);
+        System.out.println("Работник был успешно добавлен в департамент!");
+
+    }
+}
---
+++ main/java/hibernate_one_to_many_bi/entity/Employee.java
@@ -0,0 +1,51 @@
+package hibernate_one_to_many_bi.entity;
+
+import hibernate_one_to_one.entity.Detail;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "salary")
+    private int salary;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH,
+            CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
